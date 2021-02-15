b571e07281c2f19cb11bb6db2abd7f341d776bbc
Corey Smith <CSSmith1997@gmail.com>
2021-02-14 17:07:57 -0700

    Added a table component to display information from the API to

---
+++ employee-directory/components/Table/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function Table(props) {
+    return (
+        <table className = 'eTable'>
+            <head>
+                <tr>
+                    <th scope="col"><button onClick={() => props.handleSort('picture[thumbnail]')} className='btn'> Image </button></th>
+                    <th scope="col"><button onClick={() => props.handleSort('name[first]')} className='btn'>Name</button></th>
+                    <th scope="col"><button onClick={() => props.handleSort('dob[date]')} className='btn'>DOB</button></th>
+                    <th scope="col"><button onClick={() => props.handleSort('email')} className='btn'>Email</button></th>
+                    <th scope="col"><button onClick={() => props.handleSort('phone')} className='btn'>Phone</button></th>
+                </tr>
+
+
+            </head>
+        </table>
+    )
+}
